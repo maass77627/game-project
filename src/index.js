61828fc17f1df231cc00eb05f1ac0fc7703ca9fa
kelly_maass <kmaass77627@gmail.com>
2020-07-28 23:35:00 -0500

    musicplays

--- src/index.js
+++ src/index.js
@@ -4,26 +4,34 @@ background.src = 'ocean2.jpg';
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
    startGame();
+   
 });
 
 
 var myGamePiece;
 var myBackground;
+var myObstacle;
+var octopus;
+var myMusic;
 
 function startGame() {
     myGameArea.start();
     myGamePiece = new component(100, 100, "beatsub.png", 10, 120, "image");
     myBackground = new component(700, 400, "ocean2.jpg", 0, 0, "background");
+    myObstacle = new component(50, 50, "fishy.png", 300, 120, "image");
+    octopus = new component(150, 150, "octy.png", 400, 200, "image");
+     myMusic = new sound("oct.mp3");
+ // myMusic.play();
 }
 
 var myGameArea = {
-   // canvas : document.createElement("canvas"),
+  
     canvas : document.getElementById("myCanvas"),
     start : function() {
         this.canvas.width = 700;
         this.canvas.height = 400;
         this.context = this.canvas.getContext("2d");
-       // document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+      
         this.interval = setInterval(updateGameArea, 20);
         window.addEventListener('keydown', function (e) {
             myGameArea.key = e.keyCode;
@@ -77,17 +85,39 @@ var myGameArea = {
 
 function updateGameArea() {
     myGameArea.clear();
+    
     myBackground.speedX = -1;
     myBackground.newPos();
     myBackground.update();
     myGamePiece.speedX = 0;
-    myGamePiece.speedY = 0;    
+    myGamePiece.speedY = 0;   
+    if (myGameArea.key) {myMusic.play();} 
     if (myGameArea.key && myGameArea.key == 37) {myGamePiece.speedX = -1; }
     if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 1; }
     if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -1; }
     if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 1; }
+    
     myGamePiece.newPos();    
     myGamePiece.update();
+    myObstacle.x += -1;
+    myObstacle.update();
+    octopus.x += -.5;
+    octopus.update();
+}
+
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+        this.sound.play();
+    }
+    this.stop = function(){
+        this.sound.pause();
+    }    
 }
 
 
