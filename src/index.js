1df3866672fdd918108ce4ea271816c3a695b097
kelly_maass <kmaass77627@gmail.com>
2020-07-31 02:22:21 -0500

    canges

--- src/index.js
+++ src/index.js
@@ -1,40 +1,44 @@
-var background = new Image();
-background.src = 'ocean2.jpg';
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-   startGame();
-   
-});
-
-
-var myGamePiece;
-var myBackground;
-var myObstacle;
+var submarine;
+var background;
+var fish;
+var fish2;
+var fish3;
 var octopus;
-var myMusic;
+var music;
 var flowers;
 var crab;
 var jellyfish;
+var gfish;
+var bubble;
+var bubbleSound;
+//var psy;
 
 function startGame() {
     myGameArea.start();
-    myGamePiece = new component(100, 100, "beatsub.png", 10, 120, "image");
-    myBackground = new component(700, 400, "ocean2.jpg", 0, 0, "background");
-    myObstacle = new component(50, 50, "fishy.png", 800, 120, "image");
+    submarine = new component(100, 100, "beatsub.png", 10, 120, "image");
+    background = new component(700, 400, "ocean2.jpg", 0, 0, "background");
+    fish = new component(50, 50, "fishy.png", 800, 120, "image");
+    fish2 = new component(50, 50, "fishy.png", 3000, 150, "image");
+    fish3 = new component(50, 50, "fishy.png", 900, 100, "image");
     octopus = new component(150, 150, "octy.png", 1000, 200, "image");
-    myMusic = new sound("oct.mp3");
+    music = new sound("oct.mp3");
     flowers  = new component(700, 250, "flowers.png", 0, 200, "background");
     crab = new component(100, 100, "krabby.png", 1300, 300, "image");
     jellyfish = new component(100, 100, "jellyfish.png", 900, 0, "image");
- // myMusic.play();
+    gfish = new component(60, 60, "gfish.png", 900, 225, "image");
+    bubble = new component(60, 60, "bubbles.png", 500, 225, "image");
+   bubbleSound = new sound("pop.mp3");
+   // psy = new component(60, 60, "psy.png", 400, 330, "image");
+ 
+ 
 }
 
 var myGameArea = {
   
     canvas : document.getElementById("myCanvas"),
     start : function() {
-        this.canvas.width = 800;
+        this.canvas.width = 900;
         this.canvas.height = 400;
         this.context = this.canvas.getContext("2d");
       
@@ -81,6 +85,17 @@ var myGameArea = {
                 ctx.fillRect(this.x, this.y, this.width, this.height);
             }
         }
+        this.loopIt = function() {
+            if (this.x == -(this.width)) {
+                this.x = 1000;
+        }
+    }
+    this.bubbleCrash = function() {
+        if (this.x == bubble.x) {
+            //bubble.clear();
+            bubbleSound.play();
+    }
+}
         this.newPos = function() {
             this.x += this.speedX;
             this.y += this.speedY;  
@@ -93,33 +108,58 @@ var myGameArea = {
     }
 
 function updateGameArea() {
+  
     myGameArea.clear();
     
-    myBackground.speedX = -1;
-    myBackground.newPos();
-    myBackground.update();
-    myGamePiece.speedX = 0;
-    myGamePiece.speedY = 0;   
-    if (myGameArea.key) {myMusic.play();} 
-    if (myGameArea.key && myGameArea.key == 37) {myGamePiece.speedX = -1; }
-    if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 1; }
-    if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -1; }
-    if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 1; }
+    background.speedX = -1;
+    background.newPos();
+    background.update();
+
+
+    submarine.speedX = 0;
+    submarine.speedY = 0;   
+    if (myGameArea.key) {music.play();} 
+    if (myGameArea.key && myGameArea.key == 37) {submarine.speedX = -1; }
+    if (myGameArea.key && myGameArea.key == 39) {submarine.speedX = 1; }
+    if (myGameArea.key && myGameArea.key == 38) {submarine.speedY = -1; }
+    if (myGameArea.key && myGameArea.key == 40) {submarine.speedY = 1; }
     
-    myGamePiece.newPos();    
-    myGamePiece.update();
+    submarine.newPos(); 
+    submarine.bubbleCrash();   
+    submarine.update();
 
-    myObstacle.x += -3;
-    myObstacle.update();
+    fish.loopIt();
+    fish.x += -2.2;
+    fish.update();
 
+    fish2.loopIt();
+    fish2.x += -3;
+    fish2.update();
+
+    fish3.loopIt();
+    fish3.x += -2;
+    fish3.update();
+
+    octopus.loopIt();
     octopus.x += -.5;
     octopus.update();
 
+
+    crab.loopIt();
     crab.x += -2;
     crab.update();
 
+    jellyfish.loopIt();
     jellyfish.x += -2;
     jellyfish.update();
+
+    gfish.loopIt();
+    gfish.x += -2.5;
+    gfish.update();
+
+    bubble.update();
+
+   // psy.update();
 
     flowers.speedX = -1;
     flowers.newPos();
@@ -165,6 +205,11 @@ pause.addEventListener("click", function(){
 
 
 
+  document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+   startGame();
+   
+});
 
 
 
---
+++ src/index2.js
@@ -0,0 +1,13 @@
+// var score;
+
+// score = new gamepiece(50, 60, black, 50, 50, text);
+
+// function gamepiece(width, height, color, x, y, type) {
+//              this.width = width;
+//              this.height = height;
+//              this.color = color;
+
+//              this.x = x;
+//              this.y = y;
+//              this.type = type; 
+// }
